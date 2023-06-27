ef97a127dbd10e8ed998ee0afae45ffcb72d9ac0
bizarf <snesmaster40@googlemail.com>
2023-06-27 19:51:52 +0100

    fixed index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
-    res.redirect("/api/v1/user");
+    res.redirect("/api");
 });
 
 module.exports = router;
