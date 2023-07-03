6c2e23fc1e81e6944365874994d179b5853d9055
bizarf <snesmaster40@googlemail.com>
2023-07-03 16:27:08 +0100

    fix unauthorized issue with comment posting

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -14,11 +14,11 @@ exports.comment_create_post = [
 
     asyncHandler(async (req, res, next) => {
         // safe guard incase someone somehow tries to post a comment if they're not a user
-        if (!req.user) {
-            res.status(401).json({
-                error: "You are not authorized to do that",
-            });
-        }
+        // if (!req.user) {
+        //     res.status(401).json({
+        //         error: "You are not authorized to do that",
+        //     });
+        // }
 
         // check the post exists
         const post = await Post.findById(req.params.id).exec();
@@ -29,7 +29,7 @@ exports.comment_create_post = [
         const errors = validationResult(req);
 
         const comment = new Comment({
-            user: req.user._id,
+            user: req.body.userId,
             text: req.body.comment,
             postId: req.body.id,
             timestamp: new Date(),
